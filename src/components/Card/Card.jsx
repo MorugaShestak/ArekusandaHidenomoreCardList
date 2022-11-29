85e9cdd2cb755bec67f70ef5b8d32c6fcff47b70
Alexander Shestak <110448322+MorugaShestak@users.noreply.github.com>
2022-11-29 22:08:13 +0400

    Add cards & mobile adaptation

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import "./Card.css"
 import img from '../../img/Discord Profile Banner 4.png'
 
-const Card = ({name, description, cost, img}) => {
+const Card = ({name, amount, description, cost, img, type, rare}) => {
+
+    let open = () => {
+        window.open(img)
+    }
     return (
-        <div className={"card"}>
+        <div id={type} className={"card " + rare}>
             <h1 className={"name"}>{name}</h1>
-            {img ? <img className={'img'} src={img} alt="Image here"/> : ''}
+            {/*<h4>Данных карт в колоде - {amount}</h4>*/}
+            {img ? <img onClick={open} title={"Click to open full img!"} className={'img'}  src={img} alt="Image here"/> : ''}
             <h3 className={"description"}>{description}</h3>
             <h3 className={"cost"}>{cost}</h3>
         </div>
